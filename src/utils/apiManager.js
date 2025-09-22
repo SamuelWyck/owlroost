073d3df7bbeb66622b6dd496b47435df7771d13c
samuel <samawyck@gmail.com>
2025-09-22 17:45:39 -0500

    Create apiManager with methods to loging and signup

---
+++ src/utils/apiManager.js
@@ -0,0 +1,50 @@
+class ApiManager {
+    #apiDomain = "http://localhost:3000";
+
+
+    async #makeApiCall(url, options) {
+        try {
+            const res = await fetch(url, options);
+            const jsonRes = await res.json();
+            return jsonRes;
+        } catch {
+            return {
+                errors: [{msg: "Unable to connect to server"}]
+            }
+        }
+    };
+
+
+    async loginOrSignup(reqBody, signup) {
+        const endPoint = (signup) ? "signup" : "login";
+        const url = `${this.#apiDomain}/auth/${endPoint}`;
+        const options = {
+            mode: "cors",
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: reqBody
+        };
+
+        const response = await this.#makeApiCall(url, options);
+        return response;
+    };
+
+
+    async loginUser(reqBody) {
+        const res = await this.loginOrSignup(reqBody, false);
+        return res;
+    };
+
+
+    async signupUser(reqBody) {
+        const res = await this.loginOrSignup(reqBody, true);
+        return res;
+    };
+};
+
+
+
+export default new ApiManager();
