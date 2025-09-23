a981fc501dbc362203f9ed31df940d192f69272f
samuel <samawyck@gmail.com>
2025-09-23 17:17:03 -0500

    Create post form with new post logic

---
+++ src/components/postForm.jsx
@@ -0,0 +1,133 @@
+import "../styles/postForm.css";
+import { useState } from "react";
+import apiManager from "../utils/apiManager.js";
+import { useNavigate } from "react-router-dom";
+
+
+
+function PostForm(
+    {titleValue="", contentValue="", postId, edit}) {
+    const navigate = useNavigate();
+    const [showDel, setShowDel] = useState(false);
+    const [titleVal, setTitleVal] = useState(titleValue);
+    const [contentVal, setContentVal] = useState(contentValue);
+    const [errors, setErrors] = useState(null);
+
+
+    function toggleShowDel() {
+        setShowDel(function(showDel) {
+            return !showDel;
+        });
+    };
+
+
+    function handleTitleChange(event) {
+        setTitleVal(event.target.value);
+    };
+
+
+    function handleContentChange(event) {
+        setContentVal(event.target.value);
+    };
+
+
+    async function createPost(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        let reqBody = {};
+        for (let entry of formData.entries()) {
+            const [key, value] = entry;
+            reqBody[key] = value;
+        }
+        reqBody = JSON.stringify(reqBody);
+
+        const res = await apiManager.createPost(reqBody);
+        console.log(res)
+        if (res.errors) {
+            setErrors(getErrorCards(res.errors));
+            return;
+        }
+
+        navigate("/");
+    };
+
+
+    function getErrorCards(errors) {
+        const cards = [];
+        for (let error of errors) {
+            cards.push(
+                <li 
+                    className="error" 
+                    key={error.msg}
+                >{error.msg}</li>
+            );
+        }
+        return cards;
+    };
+
+
+    return (
+        <form 
+            className="post-form"
+            onSubmit={(edit) ? null : createPost}
+        >   
+            {!errors ||
+            <ul className="errors">
+                {errors}
+            </ul>
+            }
+            <div>
+                <input 
+                    type="text" 
+                    name="title" 
+                    placeholder="Title" 
+                    value={titleVal}
+                    onChange={handleTitleChange}
+                    maxLength={200}
+                    required
+                />
+            </div>
+            <div>
+                <textarea 
+                    name="content" 
+                    placeholder="Content"
+                    value={contentVal}
+                    onChange={handleContentChange}
+                    maxLength={11000}
+                    minLength={10}
+                    required
+                ></textarea>
+            </div>
+            <div>
+                {(edit) ?
+                <>
+                {(showDel) ?
+                <>
+                <button 
+                    type="button" 
+                    onClick={toggleShowDel}
+                >Cancel</button>
+                <button type="button">Delete</button>
+                </>
+                :
+                <>
+                <button 
+                    type="button"
+                    onClick={toggleShowDel}
+                >Delete</button>
+                <button>Save</button>
+                </>
+                }
+                </>
+                :
+                <button>Hoot</button>
+                }
+            </div>
+        </form>
+    );
+};
+
+
+
+export default PostForm;
