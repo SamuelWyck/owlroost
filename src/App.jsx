083ea7c4bd00f2e5df3f76a0d5fd1619fec54602
samuel <samawyck@gmail.com>
2025-09-23 11:33:37 -0500

    Hook up header and create effect to handle menu listeners

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,66 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Header from './components/header.jsx';
+import { useRef, useEffect } from 'react';
 
 
 
 function App() {
+	const headerRef = useRef(null);
+
+
+	useEffect(function() {
+		function toggleMenus(event) {
+			const userMenu = document.querySelector(
+				".user-menu"
+			);
+			if (userMenu) {
+				userMenu.classList.add("hidden");
+			}
+
+			const target = event.target;
+			if (target.matches(".header-menu")) {
+				return;
+			}
+
+			const headerMenu = document.querySelector(
+				".header-menu"
+			);
+			headerMenu.classList.add("hidden");
+		};
+
+
+		function hideHeaderMenu() {
+			if (window.innerWidth <= 500) {
+				return;
+			}
+
+			const headerMenu = document.querySelector(
+				".header-menu"
+			);
+			headerMenu.classList.add("hidden");
+		};
+
+
+
+		document.addEventListener("click", toggleMenus);
+		window.addEventListener("resize", hideHeaderMenu);
+
+		return function() {
+			document.removeEventListener(
+				"click", toggleMenus
+			);
+			window.removeEventListener(
+				"resize", hideHeaderMenu
+			)
+		};
+	}, []);
+
+
 	return (
 		<>
-		<p></p>
-		<Outlet />
+		<Header ref={headerRef} />
+		<Outlet context={headerRef} />
 		<p></p>
 		</>
 	);
