1e0c0b1cd06a853e3bbc0908541bd61290cc5c03
samuel <samawyck@gmail.com>
2025-09-24 12:30:08 -0500

    Use postCard element

--- src/components/postsPage.jsx
+++ src/components/postsPage.jsx
@@ -1,23 +1,57 @@
 import "../styles/postsPage.css";
 import { useOutletContext } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import apiManager from "../utils/apiManager.js";
+import PostCard from "./postCard.jsx";
+import {formatDate, formatNumber} from "../utils/formatters.js";
 
 
 
 function PostsPage() {
     const headerRef = useOutletContext();
+    const [posts, setPosts] = useState(null);
 
 
     useEffect(function() {
         apiManager.getPosts().then(function(res) {
+            if (res.errors) {
+                return;
+            }
+
             headerRef.current.updateUser(res.user);
+            setPosts(getPostCards(res.posts, res.user.id));
         });
     }, [headerRef]);
 
 
+    function getPostCards(posts, userId) {
+        const cards = [];
+        for (let post of posts) {
+            post.date = formatDate(post.date);
+            post.displayLikes = formatNumber(post.likes);
+            console.log(post)
+            cards.push(
+                <PostCard 
+                    post={post} 
+                    key={post.post_id} 
+                    userId={userId} 
+                />
+            );
+        }
+        return cards;
+    };
+
+
     return (
-        <p>hello world</p>
+    <main className="posts-page">
+        <div className="filter-choices">
+            <button>New</button>
+            <button>Popular</button>
+        </div>
+        <div className="posts">
+            {posts}
+        </div>
+    </main>
     );
 };
 
