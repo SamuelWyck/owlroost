0addc8620dde01f0f6e6cc60387daa6ecc4a96ae
samuel <samawyck@gmail.com>
2025-09-26 14:58:40 -0500

    Hack out logic for comment card

---
+++ src/components/commentCard.jsx
@@ -0,0 +1,92 @@
+import "../styles/commentCard.css";
+import defaultImg from "../assets/account.svg";
+import editImg from "../assets/pencil.svg";
+import deleteImg from "../assets/delete.svg";
+import closeImg from "../assets/close.svg";
+import saveImg from "../assets/save.svg";
+import { useState, useRef } from "react";
+import { formatDate } from "../utils/formatters.js";
+
+
+
+function CommentCard({comment, userId}) {
+    const dateRef = useRef(formatDate(comment.timestamp));
+    const [showDel, setShowDel] = useState(false);
+    const [showEdit, setShowEdit] = useState(false);
+
+
+    function toggleDel() {
+        setShowDel(!showDel);
+    };
+
+
+    function toggleEdit() {
+        setShowEdit(!showEdit);
+    };
+
+
+    return (
+    <div className="comment-card">
+        <div className="comment-card-info">
+            <div className="comment-card-author">
+                <div className="comment-card-img-wrapper">
+                    {(comment.profile_img_url) ?
+                    <img src={comment.profile_img_url}/>
+                    :
+                    <img src={defaultImg} className="default"/>
+                    }
+                </div>
+                <p className="comment-card-author-name">
+                    {comment.username}
+                </p>
+            </div>
+        </div>
+        <p className="comment-card-date">
+            {dateRef.current}
+        </p>
+        <p className="comment-card-content">
+            {comment.content}
+        </p>
+        {comment.author_id !== userId ||
+        <div className="comment-card-options">
+            {(!showDel && !showEdit) ?
+            <>
+            <button key={0} onClick={toggleDel}>
+                <img src={deleteImg} alt="delete" />
+            </button>
+            <button key={1} onClick={toggleEdit}>
+                <img src={editImg} alt="edit" />
+            </button>
+            </>
+            :
+            <>
+            {(showDel) ?
+            <>
+            <button key={2} onClick={toggleDel}>
+                <img src={closeImg} alt="cancel" />
+            </button>
+            <button key={3}>
+                <img src={deleteImg} alt="delete" />
+            </button>
+            </>
+            :
+            <>
+            <button key={4} onClick={toggleEdit}>
+                <img src={closeImg} alt="cancel" />
+            </button>
+            <button key={5}>
+                <img src={saveImg} alt="save" />
+            </button>
+            </>
+            }
+            </>
+            }
+        </div>
+        }
+    </div>  
+    );
+};
+
+
+
+export default CommentCard;
