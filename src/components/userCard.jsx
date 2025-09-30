1907ecff4fdec89fa2d7e4835beae5b8ecd93cca
samuel <samawyck@gmail.com>
2025-09-30 18:05:08 -0500

    Create user card

---
+++ src/components/userCard.jsx
@@ -0,0 +1,116 @@
+import "../styles/userCard.css";
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import defaultImg from "../assets/account.svg";
+import apiManager from "../utils/apiManager.js";
+
+
+
+function UserCard({user, userId}) {
+    const fetching = useRef(false);
+    const [contactMade, setContactMade] = useState(
+        checkUserContact()
+    );
+
+
+    function checkUserContact() {
+        if (!userId) {
+            return null;
+        }
+
+        for (let id of user.followed_users) {
+            if (id === userId) {
+                return true;
+            }
+        }
+        for (let id of user.following_users) {
+            if (id === userId) {
+                return true;
+            }
+        }
+        for (let id of user.sent_requests) {
+            if (id === userId) {
+                return true;
+            }
+        }
+        for (let id of user.received_requests) {
+            if (id === userId) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+
+    async function followUser(event) {
+        event.preventDefault();
+        if (fetching.current) {
+            return;
+        }
+
+        fetching.current = true;
+        const res = await apiManager.followUser(user.id);
+        fetching.current = false;
+        if (res.errors) {
+            return;
+        }
+
+        setContactMade(true);
+    };
+
+
+    async function unfollowUser(event) {
+        event.preventDefault();
+        if (fetching.current) {
+            return;
+        }
+
+        fetching.current = true;
+        const res = await apiManager.unfollowUser(user.id);
+        fetching.current = false;
+        if (res.errors) {
+            return;
+        }
+
+        setContactMade(false);
+    };
+
+
+    return (
+    <Link to={`/users/${user.id}`} className="user-card-link">
+    <div className="user-card">
+        <div className="user-info">
+            <div className="user-card-img-wrapper">
+                {(user.profile_img_url) ?
+                <img src={user.profile_img_url} />
+                :
+                <img src={defaultImg} className="default" />
+                }
+            </div>
+            <p className="user-card-username">
+                {user.username}
+            </p>
+        </div>
+        {contactMade === null ||
+        <div className="follow-wrapper">
+            {(contactMade) ?
+            <button 
+                key={0}
+                onClick={unfollowUser}
+            >Unfollow user</button>
+            :
+            <button 
+                key={1} 
+                onClick={followUser}
+            >Follow user</button>
+            }
+        </div>
+        }
+    </div>
+    </Link>
+    );
+};
+
+
+
+export default UserCard;
