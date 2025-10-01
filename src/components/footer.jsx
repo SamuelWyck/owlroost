43c420749620df3d2651348b57d946898b307f09
samuel <samawyck@gmail.com>
2025-10-01 09:39:14 -0500

    Create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import "../styles/footer.css";
+
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; owlroost</p>
+        </footer>
+    );
+};
+
+
+
+export default Footer;
