27d796c98e6c3111f2f9f565117f7fe277cc6710
samuel <samawyck@gmail.com>
2025-10-02 14:20:06 -0500

    Add logic to reject, cancel, and accept requests

--- src/components/requestCard.jsx
+++ src/components/requestCard.jsx
@@ -3,11 +3,14 @@ import defaultImg from "../assets/account.svg";
 import closeImg from "../assets/close.svg";
 import yesImg from "../assets/check.svg";
 import deleteImg from "../assets/delete.svg";
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import apiManager from "../utils/apiManager.js";
+import { ErrorContext } from "../utils/context.js";
 
 
 
-function RequestCard({request, sent}) {
+function RequestCard({request, sent, delCb}) {
+    const errorRef = useContext(ErrorContext);
     const username = useRef(
         (sent) ? request.receiving_user_username : 
         request.sending_user_username
@@ -21,6 +24,39 @@ function RequestCard({request, sent}) {
 
     function toggleDel() {
         setShowDel(!showDel);
+    };
+
+
+    async function delRequest() {
+        let res = null;
+        if (sent) {
+            res = await apiManager.unfollowUser(
+                request.receiving_user_id
+            );
+        } else {
+            res = await apiManager.rejectFollowReq(
+                request.sending_user_id
+            );
+        }
+        if (res.errors) {
+            errorRef.current.showError("Server error");
+            return;
+        }
+
+        delCb(request.request_id, sent);
+    };
+
+
+    async function acceptRequest() {
+        const res = await apiManager.acceptFollowReq(
+            request.sending_user_id
+        );
+        if (res.errors) {
+            errorRef.current.showError("Server error");
+            return;
+        }
+
+        delCb(request.request_id, sent);
     };
 
 
@@ -47,7 +83,7 @@ function RequestCard({request, sent}) {
             </button>
             <button
                 key={1}
-                onClick={null}
+                onClick={delRequest}
             >
                 <img src={deleteImg} alt="delete" />
             </button>
@@ -71,7 +107,7 @@ function RequestCard({request, sent}) {
             </button>
             <button
                 key={4}
-                onClick={4}
+                onClick={acceptRequest}
             >
                 <img src={yesImg} alt="accept" />
             </button>
